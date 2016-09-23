75107b46d2b3c63762664ee58b430dd7453afd89
Christopher Rabotin <christopher.rabotin@gmail.com>
2016-09-22 18:44:22 -0600

    Woop! Integrator works! Finally!

--- src/integrator/rk4.go
+++ src/integrator/rk4.go
@@ -1,7 +1,6 @@
 package integrator
 
 import (
-	"errors"
 	"fmt"
 	"math/big"
 )
@@ -9,38 +8,125 @@ import (
 // Integrable defines something which can be integrated, i.e. has a state vector.
 // WARNING: Implementation must manage its own state based on the iteration.
 type Integrable interface {
+	GetState() []float64                   // Get the latest state of this integrable.
+	SetState(i uint64, s []float64)        // Set the state s of a given iteration i.
+	Stop(i uint64) bool                    // Return whether to stop the integration from iteration i.
+	Func(t float64, s []float64) []float64 // ODE function from time t and state s, must return a new state.
+}
+
+// RK4 defines an RK4 integrator using math.Big Floats.
+type RK4 struct {
+	X0        float64    // The initial x0.
+	StepSize  float64    // The step size.
+	Integator Integrable // What is to be integrated.
+}
+
+// NewRK4 returns a new BigRK4 integrator instance.
+func NewRK4(x0 float64, stepSize float64, inte Integrable) (r *RK4) {
+	if stepSize <= 0 {
+		panic("config StepSize must be positive")
+	}
+	if inte == nil {
+		panic("config Integator may not be nil")
+	}
+	r = &RK4{X0: x0, StepSize: stepSize, Integator: inte}
+	return
+}
+
+// Solve solves the configured RK4.
+// Returns the number of iterations performed and the last X_i, or an error.
+func (r *RK4) Solve() (uint64, float64, error) {
+	const (
+		half     = 0.5
+		oneSixth = 1 / 6.0
+		oneThird = 1 / 3.0
+	)
+
+	iterNum := uint64(0)
+	xi := r.X0
+	for !r.Integator.Stop(iterNum) {
+		halfStep := xi * half
+		state := r.Integator.GetState()
+		newState := make([]float64, len(state))
+		k1 := make([]float64, len(state))
+		//k2, k3, k4 are used as buffers AND result variables.
+		k2 := make([]float64, len(state))
+		k3 := make([]float64, len(state))
+		k4 := make([]float64, len(state))
+		tState := make([]float64, len(state))
+
+		// Compute the k's.
+		for i, y := range r.Integator.Func(xi, state) {
+			k1[i] = y * r.StepSize
+			fmt.Printf("k1 = %4.4f\n", k1[i])
+			tState[i] = state[i] + k1[i]*half
+		}
+		for i, y := range r.Integator.Func(xi+halfStep, tState) {
+			k2[i] = y * r.StepSize
+			fmt.Printf("k2 = %4.4f\n", k2[i])
+			tState[i] = state[i] + k2[i]*half
+		}
+		for i, y := range r.Integator.Func(xi+halfStep, tState) {
+			k3[i] = y * r.StepSize
+			fmt.Printf("k3 = %4.4f\n", k3[i])
+			tState[i] = state[i] + k3[i]
+		}
+		for i, y := range r.Integator.Func(xi+halfStep, tState) {
+			k4[i] = y * r.StepSize
+			fmt.Printf("k4 = %4.4f\n", k4[i])
+		}
+		// Let's now compute the new state.
+		for i := range newState {
+			newState[i] = state[i] + oneSixth*(k1[i]+k4[i]) + oneThird*(k2[i]+k3[i])
+		}
+		r.Integator.SetState(iterNum, newState)
+
+		xi += r.StepSize
+		iterNum++ // Don't forget to increment the number of iterations.
+	}
+
+	return iterNum, xi, nil
+}
+
+/* --- *
+* Integration using math.big.
+* ---- */
+
+// BigIntegrable defines something which can be integrated, i.e. has a state vector.
+// WARNING: Implementation must manage its own state based on the iteration.
+type BigIntegrable interface {
 	GetState() []big.Float                        // Get the latest state of this integrable.
 	SetState(i uint64, s []big.Float)             // Set the state s of a given iteration i.
 	Stop(i uint64) bool                           // Return whether to stop the integration from iteration i.
 	Func(t *big.Float, s []big.Float) []big.Float // ODE function from time t and state s, must return a new state.
 }
 
-// Config defines the configuration for the integration.
-type Config struct {
-	X0               *big.Float // The initial x0.
-	StepSize         float64    // The step size.
-	Integator        Integrable // What is to be integrated.
-	stepSizeBigFloat *big.Float // The step size as a big float.
+// BigRK4 defines an RK4 integrator using math.Big Floats.
+type BigRK4 struct {
+	X0               *big.Float    // The initial x0.
+	StepSize         float64       // The step size.
+	Integator        BigIntegrable // What is to be integrated.
+	stepSizeBigFloat *big.Float    // The step size as a big float.
 }
 
-func (c *Config) verify() error {
-	if c.StepSize <= 0 {
-		return errors.New("config StepSize must be positive")
+// NewBigRK4 returns a new BigRK4 integrator instance.
+func NewBigRK4(x0 *big.Float, stepSize float64, inte BigIntegrable) (r *BigRK4) {
+
+	if stepSize <= 0 {
+		panic("config StepSize must be positive")
 	}
-	if c.Integator == nil {
-		return errors.New("config Integator may not be nil")
+	if inte == nil {
+		panic("config Integator may not be nil")
 	}
-	c.stepSizeBigFloat = big.NewFloat(float64(c.StepSize))
-	return nil
+	r = &BigRK4{X0: x0, StepSize: stepSize, Integator: inte}
+	r.stepSizeBigFloat = big.NewFloat(float64(stepSize))
+	return
 }
 
-// SolveRK4 solves the provided ODE.
+// Solve solves the configured RK4.
 // Returns the number of iterations performed and the last X_i, or an error.
-func SolveRK4(c *Config) (uint64, *big.Float, error) {
+func (c *BigRK4) Solve() (uint64, *big.Float, error) {
 	iterNum := uint64(0)
-	if err := c.verify(); err != nil {
-		return iterNum, nil, err
-	}
 
 	xi := big.NewFloat(0.0).Copy(c.X0)
 	half := big.NewFloat(.5)
--- src/integrator/rk4_test.go
+++ src/integrator/rk4_test.go
@@ -3,52 +3,47 @@ package integrator
 import (
 	"fmt"
 	"math"
-	"math/big"
 	"testing"
 )
 
 type Balbasi1D struct {
-	state []big.Float // Note that we don't have a state history here.
+	state []float64 // Note that we don't have a state history here.
 }
 
 func NewBalbasi1D() (b *Balbasi1D) {
 	b = &Balbasi1D{}
-	b.state = []big.Float{*big.NewFloat(1200.0)}
+	b.state = []float64{1200.0}
 	return
 }
 
-func (b *Balbasi1D) GetState() []big.Float {
+func (b *Balbasi1D) GetState() []float64 {
 	return b.state
 }
 
-func (b *Balbasi1D) SetState(i uint64, s []big.Float) {
+func (b *Balbasi1D) SetState(i uint64, s []float64) {
 	b.state = s
-	state, _ := b.GetState()[0].Float64()
-	fmt.Printf("Updated state to %5.5f\n", state)
+	fmt.Printf("Updated state to %5.5f\n", b.GetState()[0])
 }
 
 func (b *Balbasi1D) Stop(i uint64) bool {
 	return i*30 >= 480
 }
 
-func (b *Balbasi1D) Func(t *big.Float, s []big.Float) []big.Float {
-	rtn := make([]big.Float, 1)
-	theta, _ := s[0].Float64()
-	fmt.Printf("theta = %3.5f\t", theta)
-	val := (-2.2067 * 1e-12) * (math.Pow(theta, 4) - 81*1e8)
+func (b *Balbasi1D) Func(t float64, s []float64) []float64 {
+	fmt.Printf("theta = %3.5f\t", s[0])
+	val := []float64{(-2.2067 * 1e-12) * (math.Pow(s[0], 4) - 81*1e8)}
 	fmt.Printf("rtn[0] = %3.5f\n", val)
-	rtn[0] = *big.NewFloat(val)
-	return rtn
+	return val
 }
 
 func TestRK4In1D(t *testing.T) {
 	t.Log("Starting test")
-	BConfig := &Config{X0: big.NewFloat(1), StepSize: 30, Integator: NewBalbasi1D()}
-	iterNum, xi, err := SolveRK4(BConfig)
+	rk4 := NewRK4(1, 30, NewBalbasi1D())
+
+	iterNum, xi, err := rk4.Solve()
 	if err != nil {
 		t.Fatalf("err: %+v\n", err)
 	}
-	fmt.Printf("iterNum = %+v\nxi =%+v\n", iterNum, xi.String())
-	state, _ := BConfig.Integator.GetState()[0].Float64()
-	fmt.Printf("Final state = %5.5f\n", state)
+	fmt.Printf("iterNum = %+v\nxi =%+v\n", iterNum, xi)
+	fmt.Printf("Final state = %5.5f\n", rk4.Integator.GetState()[0])
 }
