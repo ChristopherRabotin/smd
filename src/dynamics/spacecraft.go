69cf912f3bc1c8ea3c323b8e9a938b1150034cdf
Christopher Rabotin <christopher.rabotin@gmail.com>
2016-09-29 23:15:08 -0600

    Minor change before branch change.

--- src/dynamics/spacecraft.go
+++ src/dynamics/spacecraft.go
@@ -5,3 +5,8 @@ type Spacecraft struct {
 	Name string // Name of spacecraft
 	mass uint64 // Mass of spacecraft (in kg)
 }
+
+// GetMass returns the mass of the spacecraft at a given time.
+func (s *Spacecraft) GetMass(jde float64) uint64 {
+	return s.mass
+}
