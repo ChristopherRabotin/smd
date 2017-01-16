b2165a11f7ed8becf5e2a6c4c3bd7ab9fe55ad82
Christopher Rabotin <christopher.rabotin@gmail.com>
2017-01-15 18:38:26 -0700

    But around Mars, it's wrong...

--- src/cmd/planet/main.go
+++ src/cmd/planet/main.go
@@ -6,19 +6,19 @@ import (
 )
 
 func main() {
-	start := time.Date(2016, 3, 14, 9, 31, 0, 0, time.UTC)
-	end := time.Date(2016, 10, 13, 0, 0, 0, 0, time.UTC)
+	start := time.Now().UTC()
+	end := start.Add(time.Duration(5*30.5*24) * time.Hour)
 	sc := dynamics.NewEmptySC("test", 100)
 	// OEs
 	//earth := dynamics.Earth.HelioOrbit(start)
 	//mars := dynamics.Mars.HelioOrbit(start)
 	//a := 0.5 * (earth.GetApoapsis() + mars.GetApoapsis())
-	a := 1.5 * dynamics.Earth.Radius
+	a := 1.5 * dynamics.Mars.Radius
 	e := 1e-7
 	i := 1e-7
 	Ω := 90.0
 	ω := 45.0
 	ν := 20.5
-	mss := dynamics.NewMission(sc, dynamics.NewOrbitFromOE(a, e, i, Ω, ω, ν, dynamics.Earth), start, end, false, dynamics.ExportConfig{Filename: "Inc", OE: true, Cosmo: true, Timestamp: false})
+	mss := dynamics.NewMission(sc, dynamics.NewOrbitFromOE(a, e, i, Ω, ω, ν, dynamics.Mars), start, end, false, dynamics.ExportConfig{Filename: "Inc", OE: true, Cosmo: true, Timestamp: false})
 	mss.Propagate()
 }
