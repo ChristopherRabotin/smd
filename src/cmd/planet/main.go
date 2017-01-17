f1ba81f56e3e1241c996624f771c215fc1b1438d
Christopher Rabotin <christopher.rabotin@gmail.com>
2017-01-16 17:59:02 -0700

    Very odd:
    So, Grabing the initial orbit of Earth and propagating it returns that
    its inclination is 23.... which is actually the *tilt* of Earth with
    respect to its orbital plane, since the orbital plane tilt is much much
    lower. Something is really wrong.

--- src/cmd/planet/main.go
+++ src/cmd/planet/main.go
@@ -9,18 +9,19 @@ import (
 func main() {
 	end := time.Now().UTC().Add(time.Duration(2) * time.Hour)
 	start := end.Add(time.Duration(-5*30.5*24) * time.Hour)
-	sc := dynamics.NewEmptySC("test", 100)
-	obj := dynamics.Mars
-	a := 1.5 * obj.Radius
+	sc := dynamics.NewEmptySC("inc", 100)
+	obj := dynamics.Earth
+	oI := obj.HelioOrbit(start)
+	/*a := 20 * obj.Radius
 	e := 1e-1
-	i := 97.0 //1e-1
-	Ω := 90.0
-	ω := 45.0
+	i := 1e-1
+	Ω := 1e-1 //90.0
+	ω := 1e-1 //45.0
 	ν := 20.5
-	oI := dynamics.NewOrbitFromOE(a, e, i, Ω, ω, ν, obj)
+	oI := dynamics.NewOrbitFromOE(a, e, i, Ω, ω, ν, obj)*/
 	R, V := oI.GetRV()
-	oV := dynamics.NewOrbitFromRV(R, V, obj)
+	oV := dynamics.NewOrbitFromRV(R, V, dynamics.Sun)
 	fmt.Printf("oI: %s\noV: %s\n", oI, oV)
-	mss := dynamics.NewMission(sc, oI, start, end, false, dynamics.ExportConfig{Filename: "Inc", OE: true, Cosmo: true, Timestamp: false})
+	mss := dynamics.NewMission(sc, &oI, start, end, false, dynamics.ExportConfig{Filename: "Inc", OE: true, Cosmo: true, Timestamp: false})
 	mss.Propagate()
 }
