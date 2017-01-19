bc9c44f051a697a047fac7dc06dd3a89581cdbcf
Christopher Rabotin <christopher.rabotin@gmail.com>
2017-01-19 14:30:50 -0700

    (m) Changes in PlanetTarget and prop

--- src/dynamics/prop.go
+++ src/dynamics/prop.go
@@ -98,86 +98,6 @@ func (cl GenericCL) Type() ControlLaw {
 	return cl.cl
 }
 
-// Thruster defines a thruster interface.
-type Thruster interface {
-	// Returns the minimum power and voltage requirements for this thruster.
-	Min() (voltage, power uint)
-	// Returns the max power and voltage requirements for this thruster.
-	Max() (voltage, power uint)
-	// Returns the thrust in Newtons and isp consumed in seconds.
-	Thrust(voltage, power uint) (thrust, isp float64)
-}
-
-/* Available thrusters */
-
-// PPS1350 is the Snecma thruster used on SMART-1.
-type PPS1350 struct{}
-
-// Min implements the Thruster interface.
-func (t *PPS1350) Min() (voltage, power uint) {
-	return t.Max()
-}
-
-// Max implements the Thruster interface.
-func (t *PPS1350) Max() (voltage, power uint) {
-	return 350, 2500
-}
-
-// Thrust implements the Thruster interface.
-func (t *PPS1350) Thrust(voltage, power uint) (thrust, isp float64) {
-	if voltage == 350 && power == 2500 {
-		return 89e-3, 1650
-	}
-	panic("unsupported voltage or power provided")
-}
-
-// HERMeS is based on the NASA & Rocketdyne 12.5kW demo
-type HERMeS struct{}
-
-// Min implements the Thruster interface.
-func (t *HERMeS) Min() (voltage, power uint) {
-	return t.Max()
-}
-
-// Max implements the Thruster interface.
-func (t *HERMeS) Max() (voltage, power uint) {
-	return 800, 12500
-}
-
-// Thrust implements the Thruster interface.
-func (t *HERMeS) Thrust(voltage, power uint) (thrust, isp float64) {
-	if voltage == 800 && power == 12500 {
-		return 0.680, 2960
-	}
-	panic("unsupported voltage or power provided")
-}
-
-// GenericEP is a generic EP thruster.
-type GenericEP struct {
-	thrust float64
-	isp    float64
-}
-
-// Min implements the Thruster interface.
-func (t *GenericEP) Min() (voltage, power uint) {
-	return 0, 0
-}
-
-// Max implements the Thruster interface.
-func (t *GenericEP) Max() (voltage, power uint) {
-	return 0, 0
-}
-
-// Thrust implements the Thruster interface.
-func (t *GenericEP) Thrust(voltage, power uint) (thrust, isp float64) {
-	return t.thrust, t.isp
-}
-
-// NewGenericEP returns a generic electric prop thruster.
-func NewGenericEP(thrust, isp float64) *GenericEP {
-	return &GenericEP{thrust, isp}
-}
-
 /* Let's define some control laws. */
 
 // Coast defines an thrust control law which does not thrust.
@@ -376,8 +296,8 @@ func (cl *OptimalΔOrbit) String() string {
 func (cl *OptimalΔOrbit) Control(o Orbit) []float64 {
 	thrust := []float64{0, 0, 0}
 	if !cl.Initd {
-		cl.oInit = o
 		cl.Initd = true
+		cl.oInit = o
 		if len(cl.controls) == 5 {
 			// Let's populate this with the appropriate laws, so we're resetting it.
 			cl.controls = make([]ThrustControl, 0)
