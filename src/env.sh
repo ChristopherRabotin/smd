7965c5792007f656f845f094e7cf9daf954b3f70
Christopher Rabotin <christopher.rabotin@gmail.com>
2017-01-20 21:00:44 -0700

    Getting rid of gb

--- src/env.sh
+++
@@ -1,3 +0,0 @@
-export VSOP87=/home/chris/Workspace/planetx/sim/src/data/vsop87
-export HORIZON=/home/chris/Workspace/planetx/sim/src/data/jplhorizon
-export DATAOUT=/home/chris/Workspace/planetx/sim/outputdata
