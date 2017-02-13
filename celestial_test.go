4a348eb23b99297f6f13a20f4c189069accf89a3
Christopher Rabotin <christopher.rabotin@gmail.com>
2017-02-13 11:36:08 -0700

    Issue with Venus computations

--- celestial_test.go
+++ celestial_test.go
@@ -6,6 +6,9 @@ import (
 	"os"
 	"testing"
 	"time"
+
+	"github.com/gonum/matrix/mat64"
+	"github.com/soniakeys/meeus/julian"
 )
 
 func TestCelestialObject(t *testing.T) {
@@ -48,6 +51,35 @@ func TestHelio(t *testing.T) {
 	}
 	if math.Abs(norm(h1.V())-norm(h2.V())) > 1e-4 {
 		t.Fatal("velocity changed by more than 1 m/s in a minute")
+	}
+
+	// The following position and velocities are from Dr. Davis' Lambert test cases.
+	for _, exp := range []struct {
+		jde  float64
+		R, V []float64
+		body CelestialObject
+	}{{2455450, []float64{147084764.9, -32521189.65, 467.1900914}, []float64{5.94623924, 28.97464121, -0.0007159151471}, Earth},
+		//{2455610, []float64{-88002509.16, -62680223.13, 4220331.525}, []float64{20.0705936, -28.68982987, -1.551291815}, Venus},
+		{2456300, []float64{170145121.3, -117637192.8, -6642044.272}, []float64{14.70149986, 22.00292904, 0.1001095617}, Mars},
+		{2457500, []float64{-803451694.7, 121525767.1, 17465211.78}, []float64{-2.110465959, -12.31199244, 0.09819840772}, Jupiter},
+		{2460545, []float64{130423562.1, -76679031.85, 3624.816561}, []float64{14.61294123, 25.56747613, -0.0015034455}, Earth},
+		//{2460919, []float64{19195371.67, 106029328.4, 348953.802}, []float64{-34.57913611, 6.064190776, 2.078550651}, Venus}
+	} {
+		R, V := exp.body.HelioOrbitAtJD(exp.jde).RV()
+		Rcomp := mat64.NewVector(3, R)
+		Vcomp := mat64.NewVector(3, V)
+		Rexp := mat64.NewVector(3, exp.R)
+		Vexp := mat64.NewVector(3, exp.V)
+		if !mat64.EqualApprox(Rcomp, Rexp, 1e0) {
+			Rcomp.SubVec(Rcomp, Rexp)
+			t.Logf("%s @ %s (%f) (diff)\n%+v\n\n", exp.body, julian.JDToTime(exp.jde), exp.jde, Rcomp)
+			t.Fatalf("%s in valid R", exp.body)
+		}
+		if !mat64.EqualApprox(Vcomp, Vexp, 2e-1) {
+			Vcomp.SubVec(Vcomp, Vexp)
+			t.Logf("%s @ %s (%f) (diff)\n%+v\n\n", exp.body, julian.JDToTime(exp.jde), exp.jde, Vcomp)
+			t.Fatalf("%s in valid V", exp.body)
+		}
 	}
 }
 
