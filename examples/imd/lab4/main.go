ef5dc0f01b9188e20c2477fe8d2e6556d895390f
Christopher Rabotin <christopher.rabotin@gmail.com>
2017-02-20 01:41:27 -0700

    Worked on generating a matrix file for KST
    Cannot seem to plot contour of demo file, asked about it on mailing
    list.

--- examples/imd/lab4/main.go
+++ examples/imd/lab4/main.go
@@ -16,25 +16,37 @@ func main() {
 	initLaunch := time.Date(2005, 6, 1, 0, 0, 0, 0, time.UTC)
 	arrivalPlanet := smd.Mars
 	initArrival := time.Date(2005, 12, 1, 0, 0, 0, 0, time.UTC)
-	window := 500 //days
+	launchWindow := 500  //days
+	arrivalWindow := 500 //days
 	/*** END CONFIG ***/
 
-	csvContent := fmt.Sprintf("# %s -> %s\nLaunchDay,ArrivalDay,tof,c3@%s,vInf@%s\n", initPlanet, arrivalPlanet, initPlanet.Name, arrivalPlanet.Name)
-	for launchDay := 0; launchDay < window; launchDay++ {
+	if launchWindow != arrivalWindow {
+		panic("launch and arrival window must be of same length for now")
+	}
+
+	c3Vec := make([]float64, launchWindow+arrivalWindow)
+	vInfVec := make([]float64, launchWindow+arrivalWindow)
+	tofVec := make([]float64, launchWindow+arrivalWindow)
+
+	for launchDay := 0; launchDay < launchWindow; launchDay++ {
 		launchDT := initLaunch.Add(time.Duration(launchDay*24) * time.Hour)
 		fmt.Printf("Launch date %s\n", launchDT)
 		initOrbit := initPlanet.HelioOrbit(launchDT)
 		initR := mat64.NewVector(3, initOrbit.R())
 		initV := mat64.NewVector(3, initOrbit.V())
-		for arrivalDay := 0; arrivalDay < window; arrivalDay++ {
+		for arrivalDay := 0; arrivalDay < arrivalWindow; arrivalDay++ {
 			arrivalDT := initArrival.Add(time.Duration(arrivalDay*24) * time.Hour)
 			arrivalOrbit := arrivalPlanet.HelioOrbit(arrivalDT)
 			arrivalR := mat64.NewVector(3, arrivalOrbit.R())
 
 			tof := arrivalDT.Sub(launchDT)
+			tofVec[launchDay+arrivalDay] = tof.Hours() / 24
 			Vi, Vf, _, err := smd.Lambert(initR, arrivalR, tof, smd.TType2, smd.Sun)
 			if err != nil {
 				fmt.Println(err)
+				// Zero padding
+				c3Vec[launchDay+arrivalDay] = 0
+				vInfVec[launchDay+arrivalDay] = 0
 				break
 			}
 			// Compute the c3
@@ -44,22 +56,43 @@ func main() {
 			if math.IsNaN(c3) {
 				c3 = 0
 			}
+			c3Vec[launchDay+arrivalDay] = c3
 			// Compute the v_infinity at destination
 			VInfArrival := mat64.NewVector(3, nil)
 			VInfArrival.SubVec(VInfArrival, Vf)
 			vInfArrival := mat64.Norm(VInfArrival, 2)
-			// Add to CSV
-			csvContent += fmt.Sprintf("%d,%d,%f,%f,%f\n", launchDay, arrivalDay, tof.Hours()/24, c3, vInfArrival)
+			vInfVec[launchDay+arrivalDay] = vInfArrival
 		}
 	}
 
-	// Write CSV file.
-	f, err := os.Create("./contourdata.csv")
-	if err != nil {
-		panic(err)
+	// Create matrix, which is fully sequential (i.e. one column).
+	// Vectors to create the .mat file, cf. https://kst-plot.kde.org/kst1/handbook/data-types.html#extract-mx .
+	for _, data := range []struct {
+		name string
+		vec  []float64
+	}{{"c3", c3Vec}, {"tof", tofVec}, {"vInf", vInfVec}} {
+		matContent := fmt.Sprintf("# %s -> %s\n", initPlanet, arrivalPlanet)
+		matContent += fmt.Sprintf("[MATRIX,%d,0,0,1,1]\n", launchWindow)
+		for launchDay := 0; launchDay < launchWindow; launchDay++ {
+			matContent += fmt.Sprintf("%d\n", launchDay)
+		}
+		for arrivalDay := 0; arrivalDay < arrivalWindow; arrivalDay++ {
+			matContent += fmt.Sprintf("%d\n", arrivalDay)
+		}
+
+		for i := 0; i < len(data.vec); i++ {
+			matContent += fmt.Sprintf("%f\n", data.vec[i])
+		}
+
+		// Write CSV file.
+		f, err := os.Create(fmt.Sprintf("./data-%s.dat", data.name))
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		if _, err := f.WriteString(matContent); err != nil {
+			panic(err)
+		}
 	}
-	defer f.Close()
-	if _, err := f.WriteString(csvContent); err != nil {
-		panic(err)
-	}
+
 }
