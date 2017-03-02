6278f0b39e505cfe697fdcbdb10256551051d3e0
Christopher Rabotin <christopher.rabotin@gmail.com>
2017-03-01 18:13:20 -0700

    Now only using the best spiral found with coarse search

--- cmd/spiralfndr/main.go
+++ cmd/spiralfndr/main.go
@@ -35,7 +35,7 @@ func init() {
 
 /*
  * This example shows how to find the greatest heliocentric velocity at the end of a spiral by iterating on the initial
- * true anomaly.
+ * true anomaly. The only thing that actually matters is the argument of periapsis.
  */
 
 func sc() *smd.Spacecraft {
@@ -47,15 +47,23 @@ func sc() *smd.Spacecraft {
 		[]smd.Waypoint{smd.NewToHyperbolic(nil)})
 }
 
-func initEarthOrbit(i, Ω, ω, ν float64) *smd.Orbit {
+func initEarthOrbit(ω float64) *smd.Orbit {
 	a, e := smd.Radii2ae(39300+smd.Earth.Radius, 290+smd.Earth.Radius)
+	// Uses the min *and* max values, since it only depends on the argument of periapsis.
+	var i float64 = 31
+	var Ω float64 = 330
+	var ν float64 = 210
 	return smd.NewOrbitFromOE(a, e, i, Ω, ω, ν, smd.Earth)
 }
 
 // initMarsOrbit returns the initial orbit.
-func initMarsOrbit(i, Ω, ω, ν float64) *smd.Orbit {
+func initMarsOrbit(ω float64) *smd.Orbit {
 	// Exomars TGO.
 	a, e := smd.Radii2ae(44500+smd.Mars.Radius, 426+smd.Mars.Radius)
+	// Uses the min *and* max values, since it only depends on the argument of periapsis.
+	var i float64 = 61
+	var Ω float64 = 240
+	var ν float64 = 180
 	return smd.NewOrbitFromOE(a, e, i, Ω, ω, ν, smd.Mars)
 }
 
@@ -72,11 +80,9 @@ func main() {
 		fmt.Println("step size must be positive")
 		flag.Usage()
 		return
-	} else if stepSize <= 5 {
-		fmt.Println("[WARNING] A small step size will take several days to iterate over all possibilities")
 	}
 
-	var orbitPtr func(i, Ω, ω, ν float64) *smd.Orbit
+	var orbitPtr func(ω float64) *smd.Orbit
 	planet = strings.ToLower(planet)
 	switch planet {
 	case "mars":
@@ -92,72 +98,69 @@ func main() {
 	fmt.Printf("Finding spirals leaving from %s\n", planet)
 
 	//name := "spiral-mars"
-	depart := time.Date(2018, 11, 8, 0, 0, 0, 0, time.UTC)
+	// TODO: Check influence of date on the values found for Earth!
+	var depart time.Time
+	if planet == "earth" {
+		depart = time.Date(2018, 5, 1, 0, 0, 0, 0, time.UTC)
+	} else {
+		depart = time.Date(2018, 11, 8, 0, 0, 0, 0, time.UTC)
+	}
 	chgframePath := "../refframes/chgframe.py"
 	maxV := -1e3
 	minV := +1e3
 	var maxOrbit smd.Orbit
 	var minOrbit smd.Orbit
-	a, e, _, _, _, _, _, _, _ := orbitPtr(10, 10, 10, 10).Elements()
+	a, e, _, _, _, _, _, _, _ := orbitPtr(10).Elements()
 	rslts := make(chan string, 10)
 	wg.Add(1)
 	go streamResults(a, e, fmt.Sprintf("%s-%.0fstep", planet, stepSize), rslts)
-	for i := 1.0; i < 90; i += stepSize {
-		for Ω := 0.0; Ω < 360; Ω += stepSize {
-			for ω := 0.0; ω < 360; ω += stepSize {
-				for ν := 0.0; ν < 360; ν += stepSize {
-					initOrbit := orbitPtr(i, Ω, ω, ν)
-					astro := smd.NewMission(sc(), initOrbit, depart, depart.Add(-1), smd.Cartesian, smd.Perturbations{}, smd.ExportConfig{})
-					astro.Propagate()
+	for ω := 0.0; ω < 360; ω += stepSize {
+		initOrbit := orbitPtr(ω)
+		astro := smd.NewMission(sc(), initOrbit, depart, depart.Add(-1), smd.Cartesian, smd.Perturbations{}, smd.ExportConfig{})
+		astro.Propagate()
 
-					// Run chgframe
-					// We're now done so let's convert the position and velocity to heliocentric and check the output.
-					R, V := initOrbit.RV()
-					state := fmt.Sprintf("[%f,%f,%f,%f,%f,%f]", R[0], R[1], R[2], V[0], V[1], V[2])
-					if debug {
-						fmt.Printf("\n=== RUNNING CMD ===\npython %s -t J2000 -f IAU_Earth -e \"%s\" -s \"%s\"\n", chgframePath, astro.CurrentDT.Format(time.ANSIC), state)
-					}
-					cmd := exec.Command("python", chgframePath, "-t", "J2000", "-f", "IAU_Mars", "-e", astro.CurrentDT.Format(time.ANSIC), "-s", state)
-					cmdOut, err := cmd.Output()
-					if err != nil {
-						fmt.Fprintln(os.Stderr, "Error converting orbit to helio ", err)
-						os.Exit(1)
-					}
-					out := string(cmdOut)
+		// Run chgframe
+		// We're now done so let's convert the position and velocity to heliocentric and check the output.
+		R, V := initOrbit.RV()
+		state := fmt.Sprintf("[%f,%f,%f,%f,%f,%f]", R[0], R[1], R[2], V[0], V[1], V[2])
+		if debug {
+			fmt.Printf("\n=== RUNNING CMD ===\npython %s -t J2000 -f IAU_Earth -e \"%s\" -s \"%s\"\n", chgframePath, astro.CurrentDT.Format(time.ANSIC), state)
+		}
+		cmd := exec.Command("python", chgframePath, "-t", "J2000", "-f", "IAU_Mars", "-e", astro.CurrentDT.Format(time.ANSIC), "-s", state)
+		cmdOut, err := cmd.Output()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error converting orbit to helio ", err)
+			os.Exit(1)
+		}
+		out := string(cmdOut)
 
-					// Process output
-					newState := strings.TrimSpace(string(out))
-					// Cf. https://play.golang.org/p/g-a4idjhIb
-					newState = newState[1 : len(newState)-1]
-					components := strings.Split(newState, ",")
-					var nR = make([]float64, 3)
-					var nV = make([]float64, 3)
-					for i := 0; i < 6; i++ {
-						fl, err := strconv.ParseFloat(strings.TrimSpace(components[i]), 64)
-						if err != nil {
-							panic(err)
-						}
-						if i < 3 {
-							nR[i] = fl
-						} else {
-							nV[i-3] = fl
-						}
-					}
-					vNorm := math.Sqrt(math.Pow(nV[0], 2) + math.Pow(nV[1], 2) + math.Pow(nV[2], 2))
-					// Add to TSV file
-					rslts <- fmt.Sprintf("%f,%f,%f,%f,%f\n", vNorm, i, Ω, ω, ν)
-					if vNorm > maxV {
-						maxV = vNorm
-						maxOrbit = *initMarsOrbit(i, Ω, ω, ν)
-					} else if vNorm < minV {
-						minV = vNorm
-						minOrbit = *initMarsOrbit(i, Ω, ω, ν)
-					}
-					if debug {
-						fmt.Printf("\nν=%f\t=>V=%+v\t|V|=%f\n", ν, nV, vNorm)
-					}
-				}
+		// Process output
+		newState := strings.TrimSpace(string(out))
+		// Cf. https://play.golang.org/p/g-a4idjhIb
+		newState = newState[1 : len(newState)-1]
+		components := strings.Split(newState, ",")
+		var nR = make([]float64, 3)
+		var nV = make([]float64, 3)
+		for i := 0; i < 6; i++ {
+			fl, err := strconv.ParseFloat(strings.TrimSpace(components[i]), 64)
+			if err != nil {
+				panic(err)
 			}
+			if i < 3 {
+				nR[i] = fl
+			} else {
+				nV[i-3] = fl
+			}
+		}
+		vNorm := math.Sqrt(math.Pow(nV[0], 2) + math.Pow(nV[1], 2) + math.Pow(nV[2], 2))
+		// Add to TSV file
+		rslts <- fmt.Sprintf("%f,%f\n", vNorm, ω)
+		if vNorm > maxV {
+			maxV = vNorm
+			maxOrbit = *initMarsOrbit(ω)
+		} else if vNorm < minV {
+			minV = vNorm
+			minOrbit = *initMarsOrbit(ω)
 		}
 	}
 	fmt.Printf("\n\n=== RESULT ===\n\nmaxV=%.3f km/s\t%s\nminV=%.3f km/s\t%s\n\n", maxV, maxOrbit, minV, minOrbit)
