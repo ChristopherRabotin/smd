67c225c1b4fd74ddba0bc34f55f37df6ed856a2f
Christopher Rabotin <christopher.rabotin@gmail.com>
2017-03-23 22:48:08 -0600

    Fixed a lot of issues with this resonance

--- examples/imd/hw6/main.go
+++ examples/imd/hw6/main.go
@@ -36,32 +36,31 @@ func main() {
 	earthAtEGA1 := smd.Earth.HelioOrbit(ega1)
 	ega1R := mat64.NewVector(3, earthAtEGA1.R())
 	_, VfGA1, _, _ = smd.Lambert(vga1R, ega1R, ega1.Sub(vga1), smd.TTypeAuto, smd.Sun)
-	vfloats1 := []float64{VfGA1.At(0, 0), VfGA1.At(1, 0), VfGA1.At(2, 0)}
-	ega1Orbit := smd.NewOrbitFromRV(earthAtEGA1.R(), vfloats1, smd.Sun)
-	ega1Orbit.ToXCentric(smd.Earth, ega1)
-	vInfInGA1 := ega1Orbit.V()
-	vInfOutGA1Norm := ega1Orbit.VNorm()
-	fmt.Printf("%+v\n%f km/s\n", vInfInGA1, vInfOutGA1Norm)
+	vInfInEGA1Vec := mat64.NewVector(3, nil)
+	vInfInEGA1Vec.SubVec(VfGA1, mat64.NewVector(3, earthAtEGA1.V()))
+	vInfInEGA1 := []float64{vInfInEGA1Vec.At(0, 0), vInfInEGA1Vec.At(1, 0), vInfInEGA1Vec.At(2, 0)}
+	vInfInEGA1Norm := norm(vInfInEGA1)
+	fmt.Printf("%+v\n%f km/s\n", vInfInEGA1, vInfInEGA1Norm)
 	fmt.Println("==== QUESTION 2 ====")
 	// hwQ 2
 	earthAtEGA2 := smd.Earth.HelioOrbit(ega2)
 	ega2R := mat64.NewVector(3, earthAtEGA2.R())
 	joiR := mat64.NewVector(3, smd.Jupiter.HelioOrbit(joi).R())
 	ViGA2, _, _, _ = smd.Lambert(ega2R, joiR, joi.Sub(ega2), smd.TTypeAuto, smd.Sun)
-	vfloats2 := []float64{ViGA2.At(0, 0), ViGA2.At(1, 0), ViGA2.At(2, 0)}
-	ega2Orbit := smd.NewOrbitFromRV(earthAtEGA2.R(), vfloats2, smd.Sun)
-	ega2Orbit.ToXCentric(smd.Earth, ega2)
-	vInfOutGA2 := ega2Orbit.V()
-	vInfOutGA2Norm := ega2Orbit.VNorm()
-	fmt.Printf("%+v\n%f km/s\n", vInfOutGA2, vInfOutGA2Norm)
+	vInfOutEGA2Vec := mat64.NewVector(3, nil)
+	vInfOutEGA2Vec.SubVec(ViGA2, mat64.NewVector(3, earthAtEGA2.V()))
+	vInfOutEGA2 := []float64{vInfOutEGA2Vec.At(0, 0), vInfOutEGA2Vec.At(1, 0), vInfOutEGA2Vec.At(2, 0)}
+	vInfOutEGA2Norm := norm(vInfInEGA1)
+	fmt.Printf("%+v\n%f km/s\n", vInfOutEGA2, vInfOutEGA2Norm)
 
 	fmt.Println("==== QUESTION 3 ====")
 	aResonance := math.Pow(smd.Sun.GM()*math.Pow(resonance*earthAtEGA1.Period().Seconds()/(2*math.Pi), 2), 1/3.)
 	VScSunNorm := math.Sqrt(smd.Sun.GM() * ((2 / earthAtEGA1.RNorm()) - 1/aResonance))
 	// Compute angle theta for EGA1
-	theta := math.Acos((math.Pow(VScSunNorm, 2) - math.Pow(vInfOutGA1Norm, 2) - math.Pow(earthAtEGA1.VNorm(), 2)) / (-2 * vInfOutGA1Norm * earthAtEGA1.VNorm()))
+	theta := math.Acos((math.Pow(VScSunNorm, 2) - math.Pow(vInfInEGA1Norm, 2) - math.Pow(earthAtEGA1.VNorm(), 2)) / (-2 * vInfInEGA1Norm * earthAtEGA1.VNorm()))
 	fmt.Printf("theta = %f\n", theta*r2d)
-	// Compute the VNC2ECI DCMs for EGA1 and EGA2.
+	// Compute the VNC2ECI DCMs for EGA1.
+	// WARNING: We are generating the transposed DCM because it's simpler code.
 	V := unit(earthAtEGA1.V())
 	N := unit(earthAtEGA1.H())
 	C := cross(V, N)
@@ -71,32 +70,83 @@ func main() {
 		dcmVal[i+3] = N[i]
 		dcmVal[i+6] = C[i]
 	}
-	DCM := mat64.NewDense(3, 3, dcmVal)
+	transposedDCM := mat64.NewDense(3, 3, dcmVal)
 	data := "psi\trP1\trP2\n"
 	step := (2 * math.Pi) / 10000
 	// Print when both become higher than minRadius.
 	rpsOkay := false
+	minDeltaBT := 1e12
+	minDeltaBR := 1e12
+	minDeltaRp := 1e12
+	var minBT1, minBT2, minBR1, minBR2, minAssocψ, minRp1, minRp2 float64
+	var minega1Vin, minega1Vout, minega2Vin, minega2Vout float64
+	// The following store the information for when the maximum radius of periapsis is reached for both.
+	var maxBT1, maxBT2, maxBR1, maxBR2, maxAssocψ, maxRp1, maxRp2 float64
+	var maxega1Vin, maxega1Vout, maxega2Vin, maxega2Vout float64
 	for ψ := step; ψ < 2*math.Pi; ψ += step {
 		sψ, cψ := math.Sincos(ψ)
-		vInfOutGA1VNC := []float64{vInfOutGA1Norm * math.Cos(math.Pi-theta), vInfOutGA1Norm * math.Sin(math.Pi-theta) * cψ, -vInfOutGA1Norm * math.Sin(math.Pi-theta) * sψ}
-		vInfOutGA1 := MxV33(DCM, vInfOutGA1VNC)
-		_, rP1, _, _, _, _ := smd.GAFromVinf(vInfInGA1, vInfOutGA1, smd.Earth)
+		vInfOutEGA1VNC := []float64{vInfInEGA1Norm * math.Cos(math.Pi-theta), vInfInEGA1Norm * math.Sin(math.Pi-theta) * cψ, -vInfInEGA1Norm * math.Sin(math.Pi-theta) * sψ}
+		vInfOutEGA1Eclip := MxV33(transposedDCM.T(), vInfOutEGA1VNC)
+		_, rP1, bT1, bR1, _, _ := smd.GAFromVinf(vInfInEGA1, vInfOutEGA1Eclip, smd.Earth)
 
-		vInfInGA2 := make([]float64, 3)
+		vInfInEGA2Eclip := make([]float64, 3)
 		for i := 0; i < 3; i++ {
-			vInfInGA2[i] = vInfOutGA1[i] + ega1Orbit.V()[i] - ega2Orbit.V()[i]
+			vInfInEGA2Eclip[i] = vInfOutEGA1Eclip[i] + earthAtEGA1.V()[i] - earthAtEGA2.V()[i]
 		}
-		_, rP2, _, _, _, _ := smd.GAFromVinf(vInfOutGA1, vInfInGA2, smd.Earth)
+		_, rP2, bT2, bR2, _, _ := smd.GAFromVinf(vInfInEGA2Eclip, vInfOutEGA2, smd.Earth)
 		data += fmt.Sprintf("%f\t%f\t%f\n", ψ*r2d, rP1, rP2)
 		if !rpsOkay && rP1 > minRadius && rP2 > minRadius {
 			rpsOkay = true
 			fmt.Printf("[OK ] ψ=%.6f\trP1=%.3f km\trP2=%.3f km\n", ψ*r2d, rP1, rP2)
 		}
-		if rpsOkay && (rP1 < minRadius || rP2 < minRadius) {
-			rpsOkay = false
-			fmt.Printf("[NOK] ψ=%.6f\trP1=%.3f km\trP2=%.3f km\n", ψ*r2d, rP1, rP2)
+		if rpsOkay {
+			// Compute the delta BT and BR so we can choose the smallest one.
+			if math.Abs(bT1-bT2) < minDeltaBT && math.Abs(bR1-bR2) < minDeltaBR {
+				//fmt.Printf("[NEW] deltaBt = %f => %v\tdeltaBr = %f => %v\n", math.Abs(bT1-bT2), math.Abs(bT1-bT2) < minDeltaBT, math.Abs(bR1-bR2), math.Abs(bR1-bR2) < minDeltaBR)
+				// New mins!
+				minDeltaBT = math.Abs(bT1 - bT2)
+				minDeltaBR = math.Abs(bR1 - bR2)
+				// Store them
+				minBT1 = bT1
+				minBR1 = bR1
+				minRp1 = rP1
+				minega1Vin = norm(vInfInEGA1)
+				minega1Vout = norm(vInfOutEGA1Eclip)
+				minBT2 = bT2
+				minBR2 = bR2
+				minRp2 = rP2
+				minega2Vin = norm(vInfInEGA2Eclip)
+				minega2Vout = norm(vInfOutEGA2)
+				minAssocψ = ψ
+			} else {
+				//fmt.Printf("deltaBt = %f => %v\tdeltaBr = %f => %v\n", math.Abs(bT1-bT2), math.Abs(bT1-bT2) < minDeltaBT, math.Abs(bR1-bR2), math.Abs(bR1-bR2) < minDeltaBR)
+			}
+			if math.Abs(rP1-rP2) < minDeltaRp {
+				// Just reached a new high for both rPs.
+				minDeltaRp = math.Abs(rP1 - rP2)
+				// Store them
+				maxBT1 = bT1
+				maxBR1 = bR1
+				maxRp1 = rP1
+				maxega1Vin = norm(vInfInEGA1)
+				maxega1Vout = norm(vInfOutEGA1Eclip)
+				maxBT2 = bT2
+				maxBR2 = bR2
+				maxRp2 = rP2
+				maxega2Vin = norm(vInfInEGA2Eclip)
+				maxega2Vout = norm(vInfOutEGA2)
+				maxAssocψ = ψ
+			}
+			if rP1 < minRadius || rP2 < minRadius {
+				rpsOkay = false
+				fmt.Printf("[NOK] ψ=%.6f\trP1=%.3f km\trP2=%.3f km\n", ψ*r2d, rP1, rP2)
+			}
 		}
 	}
+	fmt.Printf("=== Mins: ψ=%f ===\nEGA1: Bt=%f\tBr=%f\trP=%f\nVin=%f\tVout=%f\tdelta=%f\n\nEGA2: Bt=%f\tBr=%f\trP=%f\nVin=%f\tVout=%f\tdelta=%f\n", minAssocψ*r2d, minBT1, minBR1, minRp1, minega1Vin, minega1Vout, minega1Vout-minega1Vin, minBT2, minBR2, minRp2, minega2Vin, minega2Vout, minega2Vout-minega2Vin)
+
+	fmt.Printf("=== Max: ψ=%f ===\nEGA1: Bt=%f\tBr=%f\trP=%f\nVin=%f\tVout=%f\tdelta=%f\n\nEGA2: Bt=%f\tBr=%f\trP=%f\nVin=%f\tVout=%f\tdelta=%f\n", maxAssocψ*r2d, maxBT1, maxBR1, maxRp1, maxega1Vin, maxega1Vout, maxega1Vout-maxega1Vin, maxBT2, maxBR2, maxRp2, maxega2Vin, maxega2Vout, maxega2Vout-maxega2Vin)
+
 	// Export data
 	f, err := os.Create("./q3.tsv")
 	if err != nil {
@@ -104,6 +154,7 @@ func main() {
 	}
 	f.WriteString(data)
 	f.Close()
+
 }
 
 // Unshamefully copied from smd/math.go
@@ -132,7 +183,7 @@ func unit(a []float64) (b []float64) {
 }
 
 // MxV33 multiplies a matrix with a vector. Note that there is no dimension check!
-func MxV33(m *mat64.Dense, v []float64) (o []float64) {
+func MxV33(m mat64.Matrix, v []float64) (o []float64) {
 	vVec := mat64.NewVector(len(v), v)
 	var rVec mat64.Vector
 	rVec.MulVec(m, vVec)
