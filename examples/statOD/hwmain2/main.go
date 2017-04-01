d8432abd99227289f5295cb5099e5deb41a46aa4
Christopher Rabotin <christopher.rabotin@gmail.com>
2017-04-01 17:07:40 -0600

    Field changes to be exported

---
+++ examples/statOD/hwmain2/main.go
@@ -0,0 +1,369 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/ChristopherRabotin/gokalman"
+	"github.com/ChristopherRabotin/smd"
+	"github.com/gonum/matrix/mat64"
+)
+
+const (
+	ekfTrigger     = -15   // Number of measurements prior to switching to EKF mode.
+	ekfDisableTime = 1200  // Seconds between measurements to switch back to CKF. Set as negative to ignore.
+	sncEnabled     = false // Set to false to disable SNC.
+	sncDisableTime = 1200  // Number of seconds between measurements to skip using SNC noise.
+	sncRIC         = true  // Set to true if the noise should be considered defined in PQW frame.
+	timeBasedPlot  = false // Set to true to plot time, or false to plot on measurements.
+	smoothing      = false // Set to true to smooth the CKF.
+)
+
+var (
+	σQExponent float64
+	wg         sync.WaitGroup
+)
+
+func init() {
+	flag.Float64Var(&σQExponent, "sigmaExp", 6, "exponent for the Q sigma (default is 6, so sigma=1e-6).")
+}
+
+func main() {
+	flag.Parse()
+	// Define the times
+	startDT := time.Now()
+	endDT := startDT.Add(time.Duration(24) * time.Hour)
+	// Define the orbits
+	leo := smd.NewOrbitFromOE(7000, 0.001, 30, 80, 40, 0, smd.Earth)
+
+	// Define the stations
+	σρ := math.Pow(1e-3, 2)    // m , but all measurements in km.
+	σρDot := math.Pow(1e-3, 2) // m/s , but all measurements in km/s.
+	st1 := smd.NewStation("st1", 0, 10, -35.398333, 148.981944, σρ, σρDot)
+	st2 := smd.NewStation("st2", 0, 10, 40.427222, 355.749444, σρ, σρDot)
+	st3 := smd.NewStation("st3", 0, 10, 35.247164, 243.205, σρ, σρDot)
+	stations := []smd.Station{st1, st2, st3}
+
+	// Vector of measurements
+	measurements := []smd.Measurement{}
+	allmeasurements := make(map[time.Time]smd.Measurement)
+	numMeasurements := 0 // Easier to count them here than to iterate the map to count.
+
+	// Define the special export functions
+	export := smd.ExportConfig{Filename: "LEO", Cosmo: false, AsCSV: true, Timestamp: false}
+	export.CSVAppendHdr = func() string {
+		hdr := "secondsSinceEpoch,"
+		for _, st := range stations {
+			hdr += fmt.Sprintf("%sRange,%sRangeRate,%sNoisyRange,%sNoisyRangeRate,", st.Name, st.Name, st.Name, st.Name)
+		}
+		return hdr[:len(hdr)-1] // Remove trailing comma
+	}
+	export.CSVAppend = func(state smd.State) string {
+		Δt := state.DT.Sub(startDT).Seconds()
+		str := fmt.Sprintf("%f,", Δt)
+		θgst := Δt * smd.EarthRotationRate
+		// Compute visibility for each station.
+		for _, st := range stations {
+			measurement := st.PerformMeasurement(θgst, state)
+			if measurement.Visible {
+				// Sanity check
+				if _, exists := allmeasurements[state.DT]; exists {
+					panic(fmt.Errorf("already have a measurement for %s", state.DT))
+				}
+				allmeasurements[state.DT] = measurement
+				numMeasurements++
+				measurements = append(measurements, measurement)
+				str += measurement.CSV()
+			} else {
+				str += ",,,,"
+			}
+		}
+		return str[:len(str)-1] // Remove trailing comma
+	}
+
+	// Generate the true orbit
+	scName := "LEO"
+	smd.NewPreciseMission(smd.NewEmptySC(scName, 0), leo, startDT, endDT, smd.Perturbations{Jn: 3}, 2*time.Second, false, export).Propagate()
+
+	// Take care of the measurements:
+	fmt.Printf("\n[INFO] Generated %d measurements\n", numMeasurements)
+	// Let's mark those as the truth so we can plot that.
+	stateTruth := make([]*mat64.Vector, numMeasurements)
+	truthMeas := make([]*mat64.Vector, numMeasurements)
+	residuals := make([]*mat64.Vector, numMeasurements)
+	for measNo, measurement := range measurements {
+		orbit := make([]float64, 6)
+		R, V := measurement.State.Orbit.RV()
+		for i := 0; i < 3; i++ {
+			orbit[i] = R[i]
+			orbit[i+3] = V[i]
+		}
+		stateTruth[measNo] = mat64.NewVector(6, orbit)
+		truthMeas[measNo] = measurement.StateVector()
+	}
+	truth := gokalman.NewBatchGroundTruth(stateTruth, truthMeas)
+
+	// Perturbations in the estimate
+	estPerts := smd.Perturbations{Jn: 2}
+
+	// Initialize the KF noise
+	σQx := math.Pow(10, -2*σQExponent)
+	var σQy, σQz float64
+	if !sncRIC {
+		σQy = σQx
+		σQz = σQx
+	}
+	noiseQ := mat64.NewSymDense(3, []float64{σQx, 0, 0, 0, σQy, 0, 0, 0, σQz})
+	noiseR := mat64.NewSymDense(2, []float64{σρ, 0, 0, σρDot})
+	noiseKF := gokalman.NewNoiseless(noiseQ, noiseR)
+
+	// Take care of measurements.
+	estHistory := make([]*gokalman.HybridKFEstimate, numMeasurements)
+	stateHistory := make([]*mat64.Vector, numMeasurements) // Stores the histories of the orbit estimate (to post compute the truth)
+	estChan := make(chan (gokalman.Estimate), 1)
+	go processEst("hybridkf", estChan)
+
+	prevXHat := mat64.NewVector(6, nil)
+	prevP := mat64.NewSymDense(6, nil)
+	var covarDistance float64 = 50
+	var covarVelocity float64 = 1
+	for i := 0; i < 3; i++ {
+		prevP.SetSym(i, i, covarDistance)
+		prevP.SetSym(i+3, i+3, covarVelocity)
+	}
+
+	visibilityErrors := 0
+	var orbitEstimate *smd.OrbitEstimate
+
+	if smoothing {
+		fmt.Println("[INFO] Smoothing enabled")
+	}
+
+	if ekfTrigger < 0 {
+		fmt.Println("[WARNING] EKF disabled")
+	} else {
+		if smoothing {
+			fmt.Println("[ERROR] Enabling smooth has NO effect because EKF is enabled")
+		}
+		if ekfTrigger < 10 {
+			fmt.Println("[WARNING] EKF may be turned on too early")
+		} else {
+			fmt.Printf("[INFO] EKF will turn on after %d measurements\n", ekfTrigger)
+		}
+	}
+
+	var kf *gokalman.HybridKF
+	var prevStationName = ""
+	var prevDT time.Time
+	var ckfMeasNo = 0
+	for measNo, measurement := range measurements {
+		if !measurement.Visible {
+			panic("why is there a non visible measurement?!")
+		}
+		ΔtDuration := measurement.State.DT.Sub(prevDT)
+		Δt := ΔtDuration.Seconds() // Everything is in seconds.
+		if measNo == 0 {
+			prevDT = measurement.State.DT
+			orbitEstimate = smd.NewOrbitEstimate("estimator", measurement.State.Orbit, estPerts, measurement.State.DT, 10*time.Second)
+			var err error
+			kf, _, err = gokalman.NewHybridKF(prevXHat, prevP, noiseKF, 2)
+			if err != nil {
+				panic(fmt.Errorf("%s", err))
+			}
+		}
+		if !kf.EKFEnabled() && ckfMeasNo == ekfTrigger {
+			// Switch KF to EKF mode
+			kf.EnableEKF()
+			fmt.Printf("[INFO] #%04d EKF now enabled\n", measNo)
+		} else if kf.EKFEnabled() && ekfDisableTime > 0 && Δt > ekfDisableTime {
+			// Switch KF back to CKF mode
+			kf.DisableEKF()
+			ckfMeasNo = 0
+			fmt.Printf("[INFO] #%04d EKF now disabled (Δt=%s)\n", measNo, ΔtDuration)
+		}
+		if timeBasedPlot {
+			// Propagate and predict for each time step until next measurement.
+			for prevDT.Before(measurement.State.DT) {
+				nextDT := prevDT.Add(10 * time.Second)
+				orbitEstimate.PropagateUntil(nextDT) // This leads to Φ(ti+1, ti)
+				// Only do a prediction.
+				kf.Prepare(orbitEstimate.Φ, nil)
+				est, perr := kf.Predict()
+				if perr != nil {
+					panic(fmt.Errorf("[error] (#%04d)\n%s", measNo, perr))
+				}
+				stateEst := mat64.NewVector(6, nil)
+				R, V := orbitEstimate.State().Orbit.RV()
+				for i := 0; i < 3; i++ {
+					stateEst.SetVec(i, R[i])
+					stateEst.SetVec(i+3, V[i])
+				}
+				fmt.Printf("%s\n\n", est)
+				//fmt.Printf("%+v\n", mat64.Formatted(stateEst.T()))
+				estChan <- truth.ErrorWithOffset(measNo, est, stateEst)
+				prevDT = nextDT
+			}
+			continue
+		}
+		// Propagate the reference trajectory until the next measurement time.
+		orbitEstimate.PropagateUntil(measurement.State.DT) // This leads to Φ(ti+1, ti)
+
+		if measurement.Station.Name != prevStationName {
+			fmt.Printf("[INFO] #%04d %s in visibility of %s (T+%s)\n", measNo, scName, measurement.Station.Name, measurement.State.DT.Sub(startDT))
+			prevStationName = measurement.Station.Name
+		}
+
+		// Compute "real" measurement
+		computedObservation := measurement.Station.PerformMeasurement(measurement.Timeθgst, orbitEstimate.State())
+		if !computedObservation.Visible {
+			fmt.Printf("[WARNING] station %s should see the SC but does not\n", measurement.Station.Name)
+			visibilityErrors++
+		}
+		Htilde := computedObservation.HTilde()
+		kf.Prepare(orbitEstimate.Φ, Htilde)
+		if sncEnabled {
+			if Δt < sncDisableTime {
+				if sncRIC {
+					// Build the RIC DCM
+					rUnit := unit(orbitEstimate.Orbit.R())
+					cUnit := unit(orbitEstimate.Orbit.H())
+					iUnit := unit(cross(rUnit, cUnit))
+					dcmVals := make([]float64, 9)
+					for i := 0; i < 3; i++ {
+						dcmVals[i] = rUnit[i]
+						dcmVals[i+3] = cUnit[i]
+						dcmVals[i+6] = iUnit[i]
+					}
+					// Update the Q matrix in the PQW
+					dcm := mat64.NewDense(3, 3, dcmVals)
+					var QECI, QECI0 mat64.Dense
+					QECI0.Mul(noiseQ, dcm.T())
+					QECI.Mul(dcm, &QECI0)
+					QECISym, err := gokalman.AsSymDense(&QECI)
+					if err != nil {
+						fmt.Printf("[error] QECI is not symmertric!")
+						panic(err)
+					}
+					kf.SetNoise(gokalman.NewNoiseless(QECISym, noiseR))
+				}
+				// Only enable SNC for small time differences between measurements.
+				Γtop := gokalman.ScaledDenseIdentity(3, math.Pow(Δt, 2)/2)
+				Γbot := gokalman.ScaledDenseIdentity(3, Δt)
+				Γ := mat64.NewDense(6, 3, nil)
+				Γ.Stack(Γtop, Γbot)
+				kf.PreparePNT(Γ)
+			}
+		}
+		est, err := kf.Update(measurement.StateVector(), computedObservation.StateVector())
+		if err != nil {
+			panic(fmt.Errorf("[error] %s", err))
+		}
+		prevXHat = est.State()
+		prevP = est.Covariance().(*mat64.SymDense)
+		stateEst := mat64.NewVector(6, nil)
+		R, V := orbitEstimate.State().Orbit.RV()
+		for i := 0; i < 3; i++ {
+			stateEst.SetVec(i, R[i])
+			stateEst.SetVec(i+3, V[i])
+		}
+		stateEst.AddVec(stateEst, est.State())
+		// Compute residual
+		residual := mat64.NewVector(2, nil)
+		residual.MulVec(Htilde, est.State())
+		residual.AddScaledVec(residual, -1, est.ObservationDev())
+		residual.ScaleVec(-1, residual)
+		residuals[measNo] = residual
+
+		if smoothing {
+			// Save to history in order to perform smoothing.
+			estHistory[measNo] = est
+			stateHistory[measNo] = stateEst
+		} else {
+			// Stream to CSV file
+			estChan <- truth.ErrorWithOffset(measNo, est, stateEst)
+		}
+		prevDT = measurement.State.DT
+
+		// If in EKF, update the reference trajectory.
+		if kf.EKFEnabled() {
+			// Update the state from the error.
+			state := est.State()
+			R, V := orbitEstimate.Orbit.RV()
+			for i := 0; i < 3; i++ {
+				R[i] += state.At(i, 0)
+				V[i] += state.At(i+3, 0)
+			}
+			orbitEstimate = smd.NewOrbitEstimate("estimator", *smd.NewOrbitFromRV(R, V, smd.Earth), estPerts, measurement.State.DT, 10*time.Second)
+		}
+		ckfMeasNo++
+	}
+
+	if smoothing {
+		fmt.Println("[INFO] Smoothing started")
+		// Perform the smoothing. First, play back all the estimates backward, and then replay the smoothed estimates forward to compute the difference.
+		if err := kf.SmoothAll(estHistory); err != nil {
+			panic(err)
+		}
+		// Replay forward
+		for estNo, estimate := range estHistory {
+			estChan <- truth.ErrorWithOffset(estNo, estimate, stateHistory[estNo])
+		}
+		fmt.Println("[INFO] Smoothing completed")
+	}
+
+	close(estChan)
+	wg.Wait()
+
+	severity := "INFO"
+	if visibilityErrors > 0 {
+		severity = "WARNING"
+	}
+	fmt.Printf("[%s] %d visibility errors\n", severity, visibilityErrors)
+	// Write the residuals to a CSV file
+	fname := "hkf"
+	f, err := os.Create(fmt.Sprintf("./%s-residuals.csv", fname))
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	f.WriteString("rho,rhoDot\n")
+	for _, residual := range residuals {
+		csv := fmt.Sprintf("%f,%f\n", residual.At(0, 0), residual.At(1, 0))
+		if _, err := f.WriteString(csv); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func processEst(fn string, estChan chan (gokalman.Estimate)) {
+	wg.Add(1)
+	// We also compute the RMS here.
+	numMeasurements := 0
+	rmsPosition := 0.0
+	rmsVelocity := 0.0
+	ce, _ := gokalman.NewCustomCSVExporter([]string{"x", "y", "z", "xDot", "yDot", "zDot"}, ".", fn+".csv", 3)
+	for {
+		est, more := <-estChan
+		if !more {
+			ce.Close()
+			wg.Done()
+			break
+		}
+		numMeasurements++
+		for i := 0; i < 3; i++ {
+			rmsPosition += math.Pow(est.State().At(i, 0), 2)
+			rmsVelocity += math.Pow(est.State().At(i+3, 0), 2)
+		}
+		ce.Write(est)
+	}
+	// Compute RMS.
+	rmsPosition /= float64(numMeasurements)
+	rmsVelocity /= float64(numMeasurements)
+	rmsPosition = math.Sqrt(rmsPosition)
+	rmsVelocity = math.Sqrt(rmsVelocity)
+	fmt.Printf("=== RMS ===\nPosition = %f\tVelocity = %f\n", rmsPosition, rmsVelocity)
+}
--- station.go
+++ station.go
@@ -17,11 +17,11 @@ const (
 
 // Station defines a ground station.
 type Station struct {
-	name                string
-	R, V                []float64 // position and velocity in ECEF
-	latΦ, longθ         float64   // these are stored in radians!
-	altitude, elevation float64
-	ρNoise, ρDotNoise   *distmv.Normal // Station noise
+	Name                       string
+	R, V                       []float64 // position and velocity in ECEF
+	LatΦ, Longθ                float64   // these are stored in radians!
+	Altitude, Elevation        float64
+	RangeNoise, RangeRateNoise *distmv.Normal // Station noise
 }
 
 // PerformMeasurement returns whether the SC is visible, and if so, the measurement.
@@ -36,9 +36,9 @@ func (s Station) PerformMeasurement(θgst float64, state State) Measurement {
 		vDiffECEF[i] = (vECEF[i] - s.V[i]) / ρ
 	}
 	ρDot := mat64.Dot(mat64.NewVector(3, ρECEF), mat64.NewVector(3, vDiffECEF))
-	ρNoisy := ρ + s.ρNoise.Rand(nil)[0]
-	ρDotNoisy := ρDot + s.ρDotNoise.Rand(nil)[0]
-	return Measurement{el >= s.elevation, ρNoisy, ρDotNoisy, ρ, ρDot, θgst, state, s}
+	ρNoisy := ρ + s.RangeNoise.Rand(nil)[0]
+	ρDotNoisy := ρDot + s.RangeRateNoise.Rand(nil)[0]
+	return Measurement{el >= s.Elevation, ρNoisy, ρDotNoisy, ρ, ρDot, θgst, state, s}
 }
 
 // RangeElAz returns the range (in the SEZ frame), elevation and azimuth (in degrees) of a given R vector in ECEF.
@@ -48,8 +48,8 @@ func (s Station) RangeElAz(rECEF []float64) (ρECEF []float64, ρ, el, az float6
 		ρECEF[i] = rECEF[i] - s.R[i]
 	}
 	ρ = norm(ρECEF)
-	rSEZ := MxV33(R3(s.longθ), ρECEF)
-	rSEZ = MxV33(R2(math.Pi/2-s.latΦ), rSEZ)
+	rSEZ := MxV33(R3(s.Longθ), ρECEF)
+	rSEZ = MxV33(R2(math.Pi/2-s.LatΦ), rSEZ)
 	el = math.Asin(rSEZ[2]/ρ) * r2d
 	az = (2*math.Pi + math.Atan2(rSEZ[1], -rSEZ[0])) * r2d
 	return
@@ -73,28 +73,28 @@ func NewStation(name string, altitude, elevation, latΦ, longθ, σρ, σρDot f
 
 // Measurement stores a measurement of a station.
 type Measurement struct {
-	Visible         bool    // Stores whether or not the attempted measurement was visible from the station.
-	ρ, ρDot         float64 // Store the range and range rate
-	trueρ, trueρDot float64 // Store the true range and range rate
-	θgst            float64
-	State           State
-	Station         Station
+	Visible                  bool    // Stores whether or not the attempted measurement was visible from the station.
+	Range, RangeRate         float64 // Store the range and range rate
+	TrueRange, TrueRangeRate float64 // Store the true range and range rate
+	Timeθgst                 float64
+	State                    State
+	Station                  Station
 }
 
 // IsNil returns the state vector as a mat64.Vector
 func (m Measurement) IsNil() bool {
-	return m.ρ == m.ρDot && m.ρDot == 0
+	return m.Range == m.RangeRate && m.RangeRate == 0
 }
 
 // StateVector returns the state vector as a mat64.Vector
 func (m Measurement) StateVector() *mat64.Vector {
-	return mat64.NewVector(2, []float64{m.ρ, m.ρDot})
+	return mat64.NewVector(2, []float64{m.Range, m.RangeRate})
 }
 
 // HTilde returns the H tilde matrix for this given measurement.
 func (m Measurement) HTilde() *mat64.Dense {
-	stationR := ECEF2ECI(m.Station.R, m.θgst)
-	stationV := ECEF2ECI(m.Station.V, m.θgst)
+	stationR := ECEF2ECI(m.Station.R, m.Timeθgst)
+	stationV := ECEF2ECI(m.Station.V, m.Timeθgst)
 	xS := stationR[0]
 	yS := stationR[1]
 	zS := stationR[2]
@@ -111,24 +111,24 @@ func (m Measurement) HTilde() *mat64.Dense {
 	zDot := V[2]
 	H := mat64.NewDense(2, 6, nil)
 	// \partial \rho / \partial {x,y,z}
-	H.Set(0, 0, (x-xS)/m.ρ)
-	H.Set(0, 1, (y-yS)/m.ρ)
-	H.Set(0, 2, (z-zS)/m.ρ)
+	H.Set(0, 0, (x-xS)/m.Range)
+	H.Set(0, 1, (y-yS)/m.Range)
+	H.Set(0, 2, (z-zS)/m.Range)
 	// \partial \dot\rho / \partial {x,y,z}
-	H.Set(1, 0, (xDot-xSDot)/m.ρ+(m.ρDot/math.Pow(m.ρ, 2))*(x-xS))
-	H.Set(1, 1, (yDot-ySDot)/m.ρ+(m.ρDot/math.Pow(m.ρ, 2))*(y-yS))
-	H.Set(1, 2, (zDot-zSDot)/m.ρ+(m.ρDot/math.Pow(m.ρ, 2))*(z-zS))
-	H.Set(1, 3, (x-xS)/m.ρ)
-	H.Set(1, 4, (y-yS)/m.ρ)
-	H.Set(1, 5, (z-zS)/m.ρ)
+	H.Set(1, 0, (xDot-xSDot)/m.Range+(m.RangeRate/math.Pow(m.Range, 2))*(x-xS))
+	H.Set(1, 1, (yDot-ySDot)/m.Range+(m.RangeRate/math.Pow(m.Range, 2))*(y-yS))
+	H.Set(1, 2, (zDot-zSDot)/m.Range+(m.RangeRate/math.Pow(m.Range, 2))*(z-zS))
+	H.Set(1, 3, (x-xS)/m.Range)
+	H.Set(1, 4, (y-yS)/m.Range)
+	H.Set(1, 5, (z-zS)/m.Range)
 	return H
 }
 
 // CSV returns the data as CSV (does *not* include the new line)
 func (m Measurement) CSV() string {
-	return fmt.Sprintf("%f,%f,%f,%f,", m.trueρ, m.trueρDot, m.ρ, m.ρDot)
+	return fmt.Sprintf("%f,%f,%f,%f,", m.TrueRange, m.TrueRangeRate, m.Range, m.RangeRate)
 }
 
 func (m Measurement) String() string {
-	return fmt.Sprintf("%s@%s", m.Station.name, m.State.DT)
+	return fmt.Sprintf("%s@%s", m.Station.Name, m.State.DT)
 }
