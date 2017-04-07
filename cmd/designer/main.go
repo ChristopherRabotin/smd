6f9f8f1c135974a50d528eb7d7739550f5bee5f5
Christopher Rabotin <christopher.rabotin@gmail.com>
2017-04-07 15:17:43 -0600

    More configuration stuff, now going to work on the real deal

--- cmd/designer/main.go
+++ cmd/designer/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
+	"strconv"
 	"time"
 
 	"github.com/ChristopherRabotin/smd"
@@ -42,7 +42,7 @@ func main() {
 	prefix := viper.GetString("General.fileprefix")
 	verbose := viper.GetBool("General.verbose")
 	if verbose {
-		fmt.Printf("[info] file prefix: %s\n", prefix)
+		log.Printf("[info] file prefix: %s\n", prefix)
 	}
 	timeStepStr := viper.GetString("General.step")
 	timeStep, durErr := time.ParseDuration(timeStepStr)
@@ -50,7 +50,7 @@ func main() {
 		log.Fatalf("could not understand `step`: %s", durErr)
 	}
 	if verbose {
-		fmt.Printf("[info] time step: %s\n", timeStep)
+		log.Printf("[info] time step: %s\n", timeStep)
 	}
 	// Date time information
 	var initLaunch, maxArrival time.Time
@@ -65,7 +65,7 @@ func main() {
 		initLaunch = julian.JDToTime(initLaunchJD)
 	}
 	if verbose {
-		fmt.Printf("[info] init launch: %s\n", initLaunch)
+		log.Printf("[info] init launch: %s\n", initLaunch)
 	}
 	maxArrivalJD := viper.GetFloat64("General.until")
 	if maxArrivalJD == 0 {
@@ -77,18 +77,65 @@ func main() {
 		maxArrival = julian.JDToTime(maxArrivalJD)
 	}
 	if verbose {
-		fmt.Printf("[info] max arrival: %s\n", maxArrival)
+		log.Printf("[info] max arrival: %s\n", maxArrival)
 	}
 	// Read all the planets.
 	planets := []smd.CelestialObject{}
 	for pNo, planetStr := range viper.GetStringSlice("General.planets") {
 		planet, err := smd.CelestialObjectFromString(planetStr)
 		if err != nil {
-			log.Fatalf("could not read planet %d: %s", pNo, err)
-		}
-		if verbose {
-			fmt.Printf("[info] #%d: %s\n", pNo, planet.Name)
+			log.Fatalf("could not read planet #%d: %s", pNo, err)
 		}
 		planets = append(planets, planet)
+	}
+	// Read and compute the radii constraints
+	periapsisRadii := []float64{}
+	for pNo, periRfactorStr := range viper.GetStringSlice("General.periRFactor") {
+		periRfactor, err := strconv.ParseFloat(periRfactorStr, 64)
+		if err != nil {
+			log.Fatalf("could not read radius periapsis factor #%d: %s", pNo, err)
+		}
+		periapsisRadii = append(periapsisRadii, periRfactor*planets[pNo].Radius)
+	}
+	// Read the deltaV constraints
+	maxDeltaVs := []float64{}
+	for pNo, deltaVStr := range viper.GetStringSlice("General.maxDeltaV") {
+		deltaV, err := strconv.ParseFloat(deltaVStr, 64)
+		if err != nil {
+			log.Fatalf("could not read maximum deltaV #%d: %s", pNo, err)
+		}
+		maxDeltaVs = append(maxDeltaVs, deltaV)
+	}
+	// Now summarize the planet passages
+	if verbose {
+		for pNo, planet := range planets {
+			if pNo != len(planets)-1 {
+				log.Printf("[info] #%d: %s\trP: %f km\tdeltaV: %f km/s\n", pNo, planet.Name, periapsisRadii[pNo], maxDeltaVs[pNo])
+			} else {
+				log.Printf("[info] #%d: %s (destination)\n", pNo, planet.Name)
+			}
+		}
+	}
+	// Read departure/arrival constraints.
+	var maxC3, maxVinfArrival float64
+	if viper.IsSet("DepartureConstraints.c3") {
+		maxC3 = viper.GetFloat64("DepartureConstraints.c3")
+	}
+	if verbose {
+		if maxC3 > 0 {
+			log.Printf("[info] max c3: %f km^2/s^2\n", maxC3)
+		} else {
+			log.Println("[warn] no max c3 set")
+		}
+	}
+	if viper.IsSet("ArrivalConstraints.vInf") {
+		maxVinfArrival = viper.GetFloat64("ArrivalConstraints.vInf")
+	}
+	if verbose {
+		if maxVinfArrival > 0 {
+			log.Printf("[info] max vInf: %f km/s\n", maxVinfArrival)
+		} else {
+			log.Println("[warn] no max vInf set")
+		}
 	}
 }
