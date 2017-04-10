cbb38474d8ea5e6838448ba540956731f9247739
Christopher Rabotin <christopher.rabotin@gmail.com>
2017-04-10 17:45:34 -0600

    IMD dates and changes to the matlab scripts

--- cmd/matlab/pcpplotsVinfs.m
+++ cmd/matlab/pcpplotsVinfs.m
@@ -2,10 +2,10 @@ function [] = pcpplotsVinfs(fname, initLaunch, initArrival, initPlanet, arrivalP
 close all
 % NOTE: only difference here is the name of the input files.
 % Load data file
-vinfInit = load(sprintf('../contour-%s-vinf-init.dat', fname));
-vinfArrival = load(sprintf('../contour-%s-vinf-arrival.dat', fname));
-tof = load(sprintf('../contour-%s-tof.dat', fname));
-dates = load(sprintf('../contour-%s-dates.dat', fname));
+vinfInit = load(sprintf('../pcpplots/contour-%s-vinf-init.dat', fname));
+vinfArrival = load(sprintf('../pcpplots/contour-%s-vinf-arrival.dat', fname));
+tof = load(sprintf('../pcpplots/contour-%s-tof.dat', fname));
+dates = load(sprintf('../pcpplots/contour-%s-dates.dat', fname));
 % Transpose data because it's written that way
 vinfInit = vinfInit';
 vinfArrival = vinfArrival';
@@ -19,14 +19,18 @@ maxVinfI = round(max(max(vinfInit)));
 if maxVinfI > 4*minVinfI
     maxVinfI = 4*minVinfI;
 end
-vinfInit_contours = [minVinfI:0.25:(maxVinfI*0.3) (maxVinfI*0.3):1:maxVinfI];
+%vinfInit_contours = [minVinfI:0.25:(maxVinfI*0.3) (maxVinfI*0.3):2:maxVinfI];
+%vinfInit_contours = [minVinfI:0.1:(minVinfI+1) (minVinfI+1):1:minVinfI+5];
+vinfInit_contours = [minVinfI:1:minVinfI+9];
 
 minVinfA = round(min(min(vinfArrival)));
 maxVinfA = round(max(max(vinfArrival)));
-if maxVinfA > 4*minVinfA
-    maxVinfA = 4*minVinfA;
+if maxVinfA > 3*minVinfA
+    maxVinfA = 3*minVinfA;
 end
-vinfAr_contours = [minVinfA:0.1:(maxVinfA*0.3) (maxVinfA*0.3):1:maxVinfA];
+%vinfAr_contours = [minVinfA:0.1:(maxVinfA*0.3) (maxVinfA*0.3):1:maxVinfA];
+%vinfAr_contours = [minVinfA:0.5:(minVinfA+2) (minVinfA+2):0.5:11];
+vinfAr_contours = [minVinfA:1:minVinfA+9];
 
 %vinfInit_contours = [2 3 4 5 6 7 8 9 10 12 14 16 18 20 22 24];
 %vinfAr_contours = [2 3 4 5 6 7 8 9 10 12 14 16 18 20 22 24];
