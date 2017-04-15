61c442518b1b43381b8f2fe2647a775fa2bf48fc
Christopher Rabotin <christopher.rabotin@gmail.com>
2017-04-15 11:28:04 -0600

    Fixed generation of ephemerides

--- cmd/refframes/horizon.py
+++ cmd/refframes/horizon.py
@@ -36,11 +36,9 @@ if __name__ == '__main__':
     end_date = parsedt('{}-01-01'.format(year+1))
 
     _load_kernels_()
-    dots = 0
-    for fld in __file__.split('/'):
-        if fld == '..':
-            dots += 1
-    f = open('../../' + '/'.join(['..' for _ in range(dots)]) + '/data/horizon/' + args.planet + '-' + str(start_date.year) + '.csv', 'w')
+    outdir = os.path.dirname(os.path.abspath(__file__)) + '/../../data/horizon/'
+    path = outdir + args.planet + '-' + str(start_date.year) + '.csv'
+    f = open(path , 'w')
     prev_month = 0
     while start_date <= end_date:
         if prev_month != start_date.month:
