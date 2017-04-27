e88da0abd1117369d8b193a5461d0dbc34af1e29
Christopher Rabotin <christopher.rabotin@gmail.com>
2017-04-26 18:07:25 -0600

    Work done to rerun some locally opti ctrl sims used in final version of thesis

--- examples/thrustcmp/main.go
+++ examples/thrustcmp/main.go
@@ -101,7 +101,7 @@ func createSpacecraft(thruster thrusterType, numThrusters int, dist float64, fur
 			if departEarth {
 				waypoints = []smd.Waypoint{smd.NewOrbitTarget(smd.Mars.HelioOrbit(time.Now()), nil, smd.Naasz, smd.OptiΔaCL, smd.OptiΔiCL), smd.NewCruiseToDistance(dist, further, nil)}
 			} else {
-				waypoints = []smd.Waypoint{smd.NewOrbitTarget(smd.Earth.HelioOrbit(time.Now()), nil, smd.Naasz, smd.OptiΔaCL, smd.OptiΔiCL, smd.OptiΔeCL), smd.NewCruiseToDistance(dist, further, nil)}
+				waypoints = []smd.Waypoint{smd.NewOrbitTarget(smd.Earth.HelioOrbit(time.Now()), nil, smd.Naasz, smd.OptiΔaCL, smd.OptiΔiCL), smd.NewCruiseToDistance(dist, further, nil)}
 			}
 		} else {
 			if departEarth {
@@ -140,6 +140,7 @@ func main() {
 	}{{1, 1}, {1, 2}, {2, 1}, {2, 2}, {3, 8}, {3, 12}}
 	for _, combin := range combinations {
 		cpuChan <- true
+		wg.Add(1)
 		go run(combin.missionNo, combin.numThrusters)
 	}
 	wg.Wait()
@@ -178,7 +179,7 @@ func run(missionNo, numThrusters int) {
 		fn += "-notopti"
 	}
 	rslts := make(chan string, 10)
-	wg.Add(1)
+	//wg.Add(1)
 	go streamResults(fn, rslts)
 
 	aGTO, eGTO := smd.Radii2ae(39300+smd.Earth.Radius, 290+smd.Earth.Radius)
